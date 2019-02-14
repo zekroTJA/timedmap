315e6aab978cd16f443fea1be1586f55b4dd5247
Ringo Hoffmann <riho@live.de>
2019-02-14 13:46:57 +0100

    updated example and readme

--- examples/example.go
+++ examples/example.go
@@ -9,14 +9,27 @@ import (
 
 func main() {
 
+	// Creates a new timed map which scans for
+	// expired keys every 1 second
 	tm := timedmap.New(1 * time.Second)
 
-	tm.Set("hey", 213, 3*time.Second)
+	// Add a key "hey" with the value 213, which should
+	// expire after 3 seconds and execute the callback, which
+	// prints that the key was expired
+	tm.Set("hey", 213, 3*time.Second, func(v interface{}) {
+		log.Println("key-value pair of 'hey' has expired")
+	})
 
+	// Print key "hey" from timed map
 	printKeyVal(tm, "hey")
 
+	// Wait for 5 seconds
+	// During this time the main thread is blocked, the
+	// key-value pair of "hey" will be expired
 	time.Sleep(5 * time.Second)
 
+	// Printing value of key "hey" wil lfail because the
+	// key-value pair does not exist anymore
 	printKeyVal(tm, "hey")
 }
 
--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,5 +2,4 @@
 
 go test \
     -v \
-    -cover \
-    -benchmem
+    -cover
