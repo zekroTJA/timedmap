98c8e1d98a1261bf869e5c525b02cbe77aedf346
Ringo Hoffmann <riho@live.de>
2021-06-11 10:17:37 +0200

    remove unnecessary scripts

--- scripts/coveralls-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go get golang.org/x/tools/cmd/cover
-go get github.com/mattn/goveralls
-
-go test -v -covermode=count -coverprofile=coverage.out
-$HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
